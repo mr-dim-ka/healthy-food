0dd5df643c16189e47f65e9b12bfbeef03906b6a
mr-dim-ka <kalinin9.7@mail.ru>
2022-10-13 18:11:36 +0600

    added header and adaptive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
-// $(function () {
-	
-// });
+$(function () {
+
+	$('.header__burger').on('click', function () {
+		$('.header__burger').toggleClass('header__burger--active');
+		$('.header__menu').toggleClass('header__menu--active');
+	});
+});
