e7e956b6caca5fe96240bff6cc0329d29a280f3b
mr-dim-ka <kalinin9.7@mail.ru>
2022-10-15 15:48:03 +0600

    added section basics and adaptive basics

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,19 @@ $(function () {
 		$('.header__burger').toggleClass('header__burger--active');
 		$('.header__menu').toggleClass('header__menu--active');
 	});
+
+	$('.slider-basics').slick({
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 620,
+				settings: {
+					slidesToShow: 1,
+				}
+			}
+		]
+	});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -91,6 +91,7 @@ function style() {
 function script() {
 	return src([
 		'node_modules/jquery/dist/jquery.js',
+		'node_modules/slick-carousel/slick/slick.js',
 	])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
